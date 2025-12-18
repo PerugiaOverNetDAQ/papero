47b051924af5a7e6228e5ae29cf5473487188644
Mattia Barbanera <mattia.barbanera@pg.infn.it>
2025-12-18 09:33:43 +0000

    ADC_convert always active during readout; minor on trig format

--- logic/trigBusyLogic.vhd
+++ logic/trigBusyLogic.vhd
@@ -80,8 +80,8 @@ begin
 
   sTrigId <= cTRG_PHYS_INT  when (sIntTrigEn = '1' and sPhysCalibn = '1') else
              cTRG_CALIB_INT when (sIntTrigEn = '1' and sPhysCalibn = '0') else
-             cTRG_CALIB_EXT when (sIntTrigEn = '0' and sPhysCalibn = '1') else
-             cTRG_PHYS_EXT;--  when (sIntTrigEn = '0' and sPhysCalibn = '0');
+             cTRG_CALIB_EXT when (sIntTrigEn = '0' and sPhysCalibn = '0') else
+             cTRG_PHYS_EXT;--  when (sIntTrigEn = '0' and sPhysCalibn = '1');
 
   sBusy <= unary_and(iBUSIES_AND) or unary_or(iBUSIES_OR);
   -----------------------------------------------------------------------------
