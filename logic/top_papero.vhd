2c45eebeac74068c5ba700f4664523baa1213c2a
Stefan Frentescu <frentescu.stefan@gmail.com>
2026-01-20 10:34:41 +0100

    Fix top_papero: sTrgBusiesOr with the correct versioni of sDetIntI.aFull

--- logic/top_papero.vhd
+++ logic/top_papero.vhd
@@ -543,7 +543,7 @@ begin
 
   --!@brief Wrapper for all of the Trigger and Data Acquisition modules
   sTrgBusiesAnd   <= (others => '0');
-  sTrgBusiesOr    <= (0 => sDetIntfCntOut.busy, 1 => sDetIntfAfull, others => '0');
+  sTrgBusiesOr    <= (0 => sDetIntfCntOut.busy, 1 => sDetIntI.aFull, others => '0');
   TdaqModule_i : TdaqModule
     generic map (
       pFDI_WIDTH => cFDI_WIDTH,
